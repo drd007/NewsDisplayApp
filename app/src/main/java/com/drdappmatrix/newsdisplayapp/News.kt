7cc8426d378b4ffebe5a5f2e9833e57894001411
Deepankar <drdappmatrix@gmail.com>
2020-12-15 20:38:21 +0530

    Initial commit

---
+++ app/src/main/java/com/drdappmatrix/newsdisplayapp/News.kt
@@ -0,0 +1,11 @@
+package com.drdappmatrix.newsdisplayapp
+
+import java.net.URL
+
+data class News(
+    val title: String,
+    val author: String,
+    val url: String,
+    val imageURL: String
+) {
+}
